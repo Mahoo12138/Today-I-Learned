faffbb5b8a6cfaacb056dcf1546b5551a1efefad
Mahoo12138 <mahoo12138@outlook.com>
2022-07-21 15:23:56 +0800

    TypeScript 类型体操-06,12

--- type-challenges/test/test2.ts
+++ type-challenges/test/test2.ts
@@ -65,3 +65,50 @@ type A1 = never extends any ? string : number; // string
 
 
 type A2 = P<never> // never
+
+
+
+type testRecord = Record<"name", string> & Record<"name",number>
+
+// 互斥的值被成为 never
+type result0 = testRecord extends Record<"name",never> ? "yes" : "no"
+
+type testRecord1 = Record<string, string> & Record<string,number>
+
+export type MergeInsertions<T> =
+  T extends object
+    ? { [K in keyof T]: MergeInsertions<T[K]> }
+    : T
+    
+type result0_1 = MergeInsertions<testRecord1> // { [x: string]: never; }
+
+type result1 = testRecord1 extends Record<string,never> ? "yes" : "no"
+
+const test_record : testRecord1  = {
+  // @ts-expect-error: Type 'number' is not assignable to type 'never'
+  "name": 324
+}
+
+type testRecord2 = Record<number, string> & Record<string,number>
+
+const test_record2: testRecord2 = {
+  1: "dfsf",
+  // @ts-expect-error
+  "2": 12312
+}
+type result2_0 = testRecord2 extends Record<string, number> ? "yes" : "no"
+type result2_1 = testRecord2 extends Record<number, string> ? "yes" : "no"
+type result2_2 = testRecord2 extends Record<string | number, number> ? "yes" : "no"
+type result2_3 = testRecord2 extends Record<string, number | string> ? "yes" : "no"
+type result2_4 = testRecord2 extends Record<string | number, number | string> ? "yes" : "no"
+
+const test232 = {
+  // 1:"dsaf",
+  "2d": 321
+}
+
+const test2_0 : testRecord2 = {}
+
+test2_0[2] = "dsfas"
+
+const test2_1 : testRecord2 = test232
